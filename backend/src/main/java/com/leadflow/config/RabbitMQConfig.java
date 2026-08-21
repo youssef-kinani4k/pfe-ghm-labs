c4b025b9c821fe7dbd8277dc62f7764e3e5adbc8
youssef-kinani4k <youssef.kinani2002@gmail.com>
2026-08-21 15:26:30 +0100

    fix: RabbitMQConfig utilise le convertisseur Jackson 3 de Spring AMQP

--- backend/src/main/java/com/leadflow/config/RabbitMQConfig.java
+++ backend/src/main/java/com/leadflow/config/RabbitMQConfig.java
@@ -7,7 +7,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -63,7 +63,7 @@ public class RabbitMQConfig {
 
     @Bean
     MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 
     @Bean
